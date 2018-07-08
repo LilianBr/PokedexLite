5130e624848463a2ce2a1d19b4e29a5e3af3f0ac
Lilian Braud <lilian.braud@reseau.eseo.fr>
2018-07-09 00:54:19 +0200

    getView() deleted in Fragments classes (available in the superclass)

--- app/src/main/java/ar/certant/test/pokedexlite/activity/fragment/Abilities.java
+++ app/src/main/java/ar/certant/test/pokedexlite/activity/fragment/Abilities.java
@@ -1,7 +1,6 @@
 package ar.certant.test.pokedexlite.activity.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,18 +11,9 @@ import ar.certant.test.pokedexlite.R;
 
 public class Abilities extends Fragment {
 
-    View view;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_abilities, container, false);
-        return view;
-    }
-
-    @Nullable
-    @Override
-    public View getView() {
-        return view;
+        return inflater.inflate(R.layout.fragment_abilities, container, false);
     }
 }
